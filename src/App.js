96d4fb68257e8863884d95b5b229e05e260a7a09
Rafael Menezes <rafa.rcm@hotmail.com>
2022-10-28 21:25:33 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,132 +1,53 @@
-import {useState} from 'react'
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AtividadeForm from "./components/AtividadeForm";
+import AtividadeLista from "./components/AtividadeLista";
 
 let initialState = [
   {
     id: 1,
     prioridade: "1",
     titulo: "titulo",
-    descricao: 'Primeira Atividade'
+    descricao: "Primeira Atividade",
   },
   {
     id: 2,
     prioridade: "2",
     titulo: "titulo",
-    descricao: 'Segunda Atividade'
+    descricao: "Segunda Atividade",
   },
 ];
 
 function App() {
-    const [atividades, setAtividades] = useState(initialState);
+  const [atividades, setAtividades] = useState(initialState);
 
-    function addAtividade(e){
-      e.preventDefault();
+  function addAtividade(e) {
+    e.preventDefault();
 
-      const atividade = {
-        id: document.getElementById('id').value,
-        prioridade: document.getElementById('prioridade').value,
-        titulo: document.getElementById('titulo').value,
-        descricao: document.getElementById('descricao').value,
-      };
+    const atividade = {
+      id: document.getElementById("id").value,
+      prioridade: document.getElementById("prioridade").value,
+      titulo: document.getElementById("titulo").value,
+      descricao: document.getElementById("descricao").value,
+    };
 
-      setAtividades([...atividades, { ...atividade}]);      
-    }
+    setAtividades([...atividades, { ...atividade }]);
+  }
 
-    function detetarAtividade(id){
-      const atividadesFiltradas = atividades.filter(atividade => atividade.id != id);
-      setAtividades([...atividadesFiltradas]);
-    }
-    
-    function prioridadeLabel(param){
-      switch(param){
-        case "1":
-          return "Baixa";
-        case "2":
-          return "Normal";
-        case "3":
-          return "Alta";
-        default:
-          return "Não definido";
-      }
-    }
-    function prioridadeStyle(param, icone){
-      switch(param){
-        case "1":
-          return icone ? "smile" : "success";
-        case "2":
-          return icone ? "meh" : "dark";
-        case "3":
-          return icone ? "frown" : "warning";
-        default:
-          return "Não definido";
-      }
-    }
+  function deletarAtividade(id) {
+    const atividadesFiltradas = atividades.filter(
+      (atividade) => atividade.id !== id
+    );
+    setAtividades([...atividadesFiltradas]);
+  }
 
-    
   return (
     <>
-      <form className="row g-3">
-        <div className="col-md-6">
-          <label className="form-label">Id</label>
-          <input id="id" type="text" className="form-control" readOnly 
-           value={Math.max.apply(Math, atividades.map((item) => item.id)) + 1}/>
-        </div> 
-        <div className="col-md-6">
-          <label id="form-label">Prioridade</label>
-          <select id="prioridade" className="form-select">
-            <option defaultValue="0">Selecione</option>
-            <option value="1">Baixa</option>
-            <option value="2">Normal</option>
-            <option value="3">Alta</option>
-          </select>
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Título</label>
-          <input id="titulo" type="text" className="form-control" />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Descrição</label>
-          <input id="descricao" type="text" className="form-control" />
-        </div> 
-        <hr />
-        <div className='col-12'>
-          <button className="btn btn-outline-secondary" onClick={addAtividade}>+ Atividade</button>
-        </div>
-      </form>
-        <div className='mt-3'>            
-          {atividades.map(ativ => (
-            <div key={ativ.id} className={"card mb-2 shadow-sm border-" + prioridadeStyle(ativ.prioridade)}>
-                <div className="card-body">
-                  <div className="d-flex justify-content-between">
-                    <h5 className="card-title">
-                      <span className="badge bg-secondary me-1">
-                        {ativ.id}
-                      </span>
-                       - {ativ.titulo}
-                    </h5>
-                    <h6>Prioridade: 
-                      <span className={"ms-1 text-" + prioridadeStyle(ativ.prioridade)}>
-                        <i className={"me-1 far fa-" + prioridadeStyle(ativ.prioridade, true)}></i>
-                        {prioridadeLabel(ativ.prioridade)}
-                      </span>                    
-                    </h6>
-                  </div>                
-                  <p className="card-text">{ativ.descricao}</p> 
-                  <div className="d-flex justify-content-end pt-2 m-0 border-top">
-                    <button className="btn btn-sm btn-outline-primary me-2">
-                      <i className="fas fa-pen me-2"></i>
-                      Editar
-                    </button>
-                    <button className="btn btn-sm btn-outline-danger" 
-                        onClick={() => detetarAtividade(ativ.id)}>
-                        <i className="fas fa-trash me-2"></i>
-                        Deletar
-                    </button>
-                  </div>                
-                </div>
-            </div>
-          ))}            
-        </div>
+      <AtividadeForm addAtividade={addAtividade} atividades={atividades} />
+      <AtividadeLista
+        atividades={atividades}
+        deletarAtividade={deletarAtividade}
+      />
     </>
   );
 }
